8849cdb695480b006c67ae0e448edec187752ea6
wilmerjsp <wil.jsp17@gmail.com>
2022-11-05 14:17:17 -0500

    Estructura Inicial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './public/**/*.html'
+  ],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
